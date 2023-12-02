e338c95fb900ea918cd5ca47f339c8b34f3492d0
samAricha <samaricha001@gmail.com>
2023-12-02 13:39:43 +0300

    day1 2022 simple version done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,42 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: String) = input.split("\n\n").map { elf ->
+        elf.lines().map { it.toInt() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun fun1(input: String):List<List<Int>> {
+        val data = parseInput(input)
+        return data
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun fun2(input: String) : Int {
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+        val data = parseInput(input)
+        val sumList = data.map{it.sum()}
+        return sumList.sortedDescending()
+            .take(3)
+            .sum()
+    }
+
+
+    val testFileInput = readFileInput("Day01_test")
+    val fileInput = readFileInput("Day01")
+
+    val testLargeSum = fun1(testFileInput).maxOf { it.sum() }
+    val largeSum = fun1(fileInput).maxOf { it.sum() }
+
+    val testTop3Sum = fun2(testFileInput)
+    val top3Sum = fun2(fileInput)
+    println(top3Sum)
+
+    val output1 = testFileInput.split("\n\n")
+//    println(output1)
+    val output2 = output1.map { it.split("\n") }
+//    println(output2)
+//    print(largeSum)
+//    check(largeSum == 24000)
+
+
+
 }
